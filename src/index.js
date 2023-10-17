add5a23c363bcaf9f37c791fbe4e7786ba10309c
merkelgonzalo <merkelgonzalo.ok@gmail.com>
2023-10-17 12:12:44 -0300

    Animaciones de imagenes y texto

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// esta funcion comprueba si un elemento esta visible en pantalla
+function isVisible(elm) {
+	var rect = elm.getBoundingClientRect();
+	var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+	return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+// cuando se carga la página...
+window.addEventListener('DOMContentLoaded', (ev0) => {
+        // asignamos un evento scroll...
+	window.addEventListener('scroll', (ev1) => {
+                // y a todos los elementos con la clase paused...
+		document.querySelectorAll(".paused").forEach(elm => {
+			if (isVisible(elm)) // que sean visibles...
+				elm.classList.remove("paused"); // les quitamos la clase paused
+		})
+	});
+});
